d3d3381366f9c2f4db17518a8caf07e9ec1f4b16
take.y <yuuki.bado1002@gmail.com>
2022-09-25 00:41:35 +0900

    add tags by jsx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import './App.css'
  */
 export const App = () => {
   return (
-    <header>DogsApp</header>
+    <>
+      <header>DogsApp</header>
+      <p className="descript">犬の画像を表示するサイトです。</p>
+      <img src="https://images.dog.ceo/breeds/mountain-swiss/n02107574_2033.jpg" title="犬の画像" alt="犬の画像" />
+    </>
   )
 }
