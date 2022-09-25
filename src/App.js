8141675eceafe0126ba2d33547973ba949e79d95
take.y <yuuki.bado1002@gmail.com>
2022-09-25 12:17:43 +0900

    add changeHandle

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = React.useState('https://images.dog.ceo/breeds/mountain-swiss/n02107574_2033.jpg');
+  
+  const handleChange = () => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg');
+  
   return (
     <>
       <header>DogsApp</header>
       <p className="descript">犬の画像を表示するサイトです。</p>
       <img src={dogUrl} title="犬の画像" alt="犬の画像" />
+      <button onClick={handleChange}>更新</button>
     </>
   )
 }
