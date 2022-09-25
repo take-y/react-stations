5bc3dc7b65091e7af0701215f02c48a52adfe99a
take.y <yuuki.bado1002@gmail.com>
2022-09-25 11:59:19 +0900

    add state of react

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState('https://images.dog.ceo/breeds/mountain-swiss/n02107574_2033.jpg');
   return (
     <>
       <header>DogsApp</header>
       <p className="descript">犬の画像を表示するサイトです。</p>
-      <img src="https://images.dog.ceo/breeds/mountain-swiss/n02107574_2033.jpg" title="犬の画像" alt="犬の画像" />
+      <img src={dogUrl} title="犬の画像" alt="犬の画像" />
     </>
   )
 }
